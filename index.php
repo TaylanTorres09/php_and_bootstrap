4be650f354a4d58e41ffd50af9880e6d3a2caa3b
TaylanTorres09 <taylantorres@gmail.com>
2023-06-25 12:17:28 -0300

    navbar-fixed-top

--- index.php
+++ index.php
@@ -12,10 +12,10 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-default navbar-fixed-top">
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon">Toggle navigation</span>
+                <span class="navbar-toggler-icon"></span>
             </button>
             <a class="navbar-brand" href="#">Projeto DEV Web</a>
             <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
