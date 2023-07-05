d5df97a3f22654761ab39762d366b2ea34ee418d
TaylanTorres09 <taylantorres@gmail.com>
2023-07-05 13:13:14 -0300

    footer

--- index.php
+++ index.php
@@ -220,6 +220,10 @@
                 </div>
             </div>
         </section>
+
+        <footer>
+            <p class="text-center">Todos os direitos reservados!</p>
+        </footer>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
